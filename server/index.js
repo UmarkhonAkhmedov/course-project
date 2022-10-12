da1de13adf0eb5184732d9523e29e85383f8ee00
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-10-12 20:48:24 +0500

    Adding prisma

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
 import cors from "cors";
 import connection from "./db/connect.js";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
+async function main() {
+  const post = await prisma.post.create({
+    data: {
+      title: "My First post",
+      body: "My first post body",
+    },
+  });
+  console.log(post);
+}
+main();
 connection();
 
 const app = express();
