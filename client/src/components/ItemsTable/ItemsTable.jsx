f0bfee2dd46c015ac855edef856815f330cd046b
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-10-28 01:53:33 +0500

    Adding Editing functionality for list of items

--- client/src/components/ItemsTable/ItemsTable.jsx
+++ client/src/components/ItemsTable/ItemsTable.jsx
@@ -3,11 +3,29 @@ import { Box, Button } from "@mui/material";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./itemsTable.css";
+import InputField from "../Form/InputField";
 
 function ItemsTable({ fetching }) {
   const { id } = useParams();
   const [select, setSelect] = useState([]);
   const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const filteredData = data.filter((item) => item.collectionsId === id);
+  const editElement = select[0];
+  // const editElementFiltered = filteredData.filter((item) => {
+  //   if (item.id === editElement) return item;
+  // });
+
+  const [items, setItems] = useState({
+    name: "",
+    tags: "",
+    collectionsId: id,
+    integerField: {},
+    stringField: {},
+    multilineField: {},
+    checkboxesField: {},
+    dateField: {},
+  });
 
   const fetchData = async () => {
     await axios.get("http://localhost:8000/items").then((res) => {
@@ -17,8 +35,6 @@ function ItemsTable({ fetching }) {
   useEffect(() => {
     fetchData();
   }, [fetching]);
-
-  const filteredData = data.filter((item) => item.collectionsId === id);
 
   const handleChange = (event, itemId) => {
     if (event.target.checked) {
@@ -50,6 +66,41 @@ function ItemsTable({ fetching }) {
       } catch (error) {
         console.log(error);
       }
+    }
+  };
+
+  const handleEditProperty = async () => {
+    try {
+      const url = `http://localhost:8000/items/update/${editElement}`;
+      const res = await axios.patch(url, items);
+
+      if (res.status === 200 || res.status === 201) {
+        setSelect([]);
+        fetchData();
+        setOpen(false);
+        setItems({
+          name: "",
+          tags: "",
+          collectionsId: id,
+          integerField: {},
+          stringField: {},
+          multilineField: {},
+          checkboxesField: {},
+          dateField: {},
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const inputHandleChange = ({ currentTarget: input }) => {
+    setItems({ ...items, [input.name]: input.value });
+  };
+
+  const editClickButton = () => {
+    if (select.length !== 0) {
+      setOpen(!open);
     }
   };
 
@@ -94,6 +145,17 @@ function ItemsTable({ fetching }) {
             variant="contained"
             className="delete__btn"
             sx={{
+              backgroundColor: "green",
+              "&:hover": { backgroundColor: "green" },
+            }}
+            onClick={editClickButton}
+          >
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            className="delete__btn"
+            sx={{
               backgroundColor: "red",
               "&:hover": { backgroundColor: "red" },
             }}
@@ -101,6 +163,37 @@ function ItemsTable({ fetching }) {
           >
             Delete
           </Button>
+        </div>
+        <div>
+          {open && (
+            <div style={{ marginTop: "20px" }}>
+              <h2>Edit Item</h2>
+              <Box sx={{ width: "50%" }}>
+                <h3 style={{ margin: "30px 0 10px 0" }}>
+                  Edit This ID: {editElement}
+                </h3>
+                <InputField
+                  name="name"
+                  value={items.name}
+                  label="Edit Name"
+                  handleChange={inputHandleChange}
+                />
+                <InputField
+                  name="tags"
+                  value={items.tags}
+                  label="Edit Tags"
+                  handleChange={inputHandleChange}
+                />
+              </Box>
+              <Button
+                variant="contained"
+                sx={{ marginTop: "20px" }}
+                onClick={handleEditProperty}
+              >
+                Save
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </Box>
