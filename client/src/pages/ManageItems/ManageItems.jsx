e47104156bbabc1c8b6706795e1139223e6c36ea
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-10-27 22:14:19 +0500

    Making some changes

--- client/src/pages/ManageItems/ManageItems.jsx
+++ client/src/pages/ManageItems/ManageItems.jsx
@@ -1,54 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import React, { useState } from "react";
+import ItemsTable from "../../components/ItemsTable/ItemsTable";
+import ModalItems from "../../components/ModalItems/ModalItems";
 import "./manageItems.css";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 300 },
-  { field: "name", headerName: "Name", width: 200 },
-  { field: "tags", headerName: "Tags", width: 200 },
-  { field: "img", headerName: "Image", width: 200 },
-  { field: "createdAt", headerName: "Created Time", width: 200 },
-];
-
 function ManageItems() {
-  const { id } = useParams();
-  const [data, setData] = useState([]);
-
-  const fetchData = async () => {
-    await axios.get("http://localhost:8000/items").then((res) => {
-      setData(res.data);
-    });
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const filteredData = data.filter((item) => item.collectionsId === id);
-
-  console.log(data);
+  const [fetching, setFetching] = useState(true);
 
   return (
-    <Box sx={{ margin: "50px 0" }}>
+    <div className="manageItems">
       <div className="container">
-        {/* {data
-          .filter((object) => object.collectionsId === id)
-          .map((item, id) => (
-            <div key={id}>{item.name}</div>
-          ))} */}
-        <div className="table">
-          <DataGrid
-            rows={filteredData}
-            columns={columns}
-            pageSize={5}
-            rowsPerPageOptions={[5]}
-            checkboxSelection
-          />
+        <div className="manageItems__header">
+          <h2>All Items</h2>
+          <ModalItems fetching={fetching} setFetching={setFetching} />
         </div>
+        <ItemsTable />
       </div>
-    </Box>
+    </div>
   );
 }
 
