c553a8b87428b103618bdf9e909ed7cfca75a130
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-10-31 17:20:05 +0500

    Creating the largest collections component

---
+++ client/src/components/Home/LargestCollections.jsx
@@ -0,0 +1,40 @@
+import { Button, Paper } from "@mui/material";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./largestCollections.css";
+
+function LargestCollections() {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    await axios.get("http://localhost:8000/collections").then((res) => {
+      setData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="largest">
+      <h2>The List Of the Largest Collections</h2>
+      <div className="largest__list">
+        {data.map((item, index) => (
+          <Paper className="largest__item" elevation={5}>
+            <div>
+              <img className="largest__item--img" src="" />
+              <h4>{item.name}</h4>
+              <p>{item.description}</p>
+              <p className="largest__item--number">Number of Items:</p>
+              <Button variant="outlined" sx={{ marginTop: "20px" }}>
+                See Items
+              </Button>
+            </div>
+          </Paper>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LargestCollections;
