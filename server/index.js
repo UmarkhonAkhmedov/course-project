07f46683e9f35acf2c72cc42d6f93438610900ae
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-11-01 18:47:00 +0500

    Changes

--- server/index.js
+++ server/index.js
@@ -30,6 +30,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = 8080;
 
 app.listen(PORT, () => console.log(`Listening on port: 8000`));
